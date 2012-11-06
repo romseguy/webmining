e7a24eb246674ebc9c4b52928cfec82fed18bb29
r.seguy <romain.seguy@uha.fr>
2012-11-06 11:10:02 +0100

    Dossier sources et import

--- src/Extractor.java
+++ src/Extractor.java
@@ -1,10 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class Extractor {
 	StringBuilder episodes;
 	
-	public String getEpisodes() {
+	public void getEpisodes() throws IOException {
 		URL oracle = new URL("http://germain-forestier.info/cours/bi/tp/episodes.html");
 		URLConnection yc = oracle.openConnection();
 		BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
@@ -15,13 +21,11 @@ public class Extractor {
 		while ((inputLine = in.readLine()) != null) {		
 			episodes.append(inputLine);
 		}
-		
-		return output;
 	}
 	
-	public Episode[] instanciateEpisodes() {
+	public void instanciateEpisodes() {
 		Pattern p = Pattern.compile("<a.+itemprop=\"name\">([^<]+)</a>");
-		Matcher matcher = Pattern.matcher(episodes.toString());
+		Matcher matcher = p.matcher(episodes.toString());
 		
 		while (matcher.find()) {
 			System.out.println(matcher.group(1));
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.io.IOException;
+
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Extractor e = new Extractor();
 		e.getEpisodes();
